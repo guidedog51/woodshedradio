82406a57667e36b65871f09233f0c430695a10bb
guidedog51 <nordl2@pacbell.net>
2015-06-11 17:36:21 -0700

    implement soundkick and echonest API's; generate ComingSoon list on landing page

--- public/scripts/echonestAPI.js
+++ public/scripts/echonestAPI.js
@@ -1,39 +1,85 @@
-function fetchArtistsByLocation(locale) {
-    var url = ECHO_ENDPOINT + 'artist/search';
-    $("#playBar").empty();
-    $.getJSON(url, 
-        { 
-            'api_key' : ECHO_API_KEY,
-            'artist_location': locale, 
-            'results' : 100,
-            'bucket': [ 'artist_location'],  
-            'sort': 'hotttnesss-desc'
-        },
-        function(data) {
-            if (data.response.status.code == 0) {
-                var artists = data.response.artists;
-                if (artists.length > 0) {
-                    for (var i = 0; i < artists.length; i++) {
-                        var artist = artists[i];
-                        var li = $("<li>");
-                        if ('artist_location' in artist) {
-                            //li.text(artist.name + " from " + artist.artist_location.location);
-                            li.text(artist.name);
-                            $("#playBar").append(li);
+var EchoNestAPI = (function () {
+        
+        
+     //private functions   
+     var _fetchArtistsByLocation =  function (locale) {
+            var url = ECHO_ENDPOINT + 'artist/search';
+            $("#playBar").empty();
+            $.getJSON(url, 
+                { 
+                    'api_key' : ECHO_API_KEY,
+                    'artist_location': locale, 
+                    'results' : 100,
+                    'bucket': [ 'artist_location'],  
+                    'sort': 'hotttnesss-desc'
+                },
+                function(data) {
+                    if (data.response.status.code == 0) {
+                        var artists = data.response.artists;
+                        if (artists.length > 0) {
+                            for (var i = 0; i < artists.length; i++) {
+                                var artist = artists[i];
+                                var li = $("<li>");
+                                if ('artist_location' in artist) {
+                                    //li.text(artist.name + " from " + artist.artist_location.location);
+                                    li.text(artist.name);
+                                    $("#playBar").append(li);
+                                } else {
+                                    console.log(artist);
+                                }
+                            }
                         } else {
-                            console.log(artist);
+                                $("#playBar").text("No results");
                         }
+                    } else {
+                        alert("Trouble getting artists: " + data.response.status.message);
                     }
-                } else {
-                        $("#playBar").text("No results");
-                }
-            } else {
-                alert("Trouble getting artists: " + data.response.status.message);
-            }
-        })
-        .error( 
-            function(data) {
-                alert("query syntax error. Use 'city:', 'region:' and 'country:' qualifiers only");
-            }
-        );
-}
+                })
+                .error( 
+                    function(data) {
+                        alert("query syntax error. Use 'city:', 'region:' and 'country:' qualifiers only");
+                    }
+                );
+        };
+        
+        var _fetchArtistsByAppearanceDate = function (start, end) {
+            var url = SOUNDKICK_ENDPOINT;
+            //$("#playBar").empty();
+            $.getJSON(url, 
+                { 
+                    'apikey' : SOUNDKICK_API_KEY,
+                    'min_date': start,  
+                    'max_date': end
+                },
+                function(data) {
+                    if (data.resultsPage.status == 'ok') {
+                        var events = data.resultsPage.results.event;
+                        if (events.length > 0) {
+                            for (i = 0; i < events.length; i++) {
+                                var event = events[i].displayName;
+                                var li = $("<li>");
+                                li.text(event);
+                                $("#playBar").append(li);
+                            }
+                            
+                        } else {
+                            $('#playBar').text("No events");     
+                      }
+                    } else {
+                        alert("Trouble getting artists: " + data.response.status.message);
+                    }
+                })
+                .error( 
+                    function(data) {
+                        alert("query syntax");
+                    }
+                );
+
+        }
+        
+        return {
+            fetchArtistsByLocation: _fetchArtistsByLocation,
+            fetchArtistsByAppearanceDate: _fetchArtistsByAppearanceDate
+        }
+     
+})();
--- public/scripts/shell.js
+++ public/scripts/shell.js
@@ -1,20 +1,16 @@
 var ECHO_API_KEY= 'M8TCQASXDTEGU9VRO';
+var SOUNDKICK_API_KEY = 'xdy5yMc0BaLlDZ0V';
 var ECHO_ENDPOINT = 'http://developer.echonest.com/api/v4/';
+var SOUNDKICK_ENDPOINT = 'http://api.songkick.com/api/3.0/metro_areas/26330-us-sf-bay-area/calendar.json';
 jQuery.ajaxSettings.traditional = true;
 
 $(document).ready(function() {
-//    var echonest = new EchoNest(ECHO_SECRET);
-    
-    //append play tag
-    // get a set of "Hybrid" audio and output as HTML5 audio tags to the page
-//    echonest.artist("Black Keys").audio( function(audioCollection) {
-//        $("#playBar").append( audioCollection.to_html('<p>${artist} - ${length} long<br /><audio src="${url}" controls preload="none"></audio></p>') );
-//    });
-//    
+
     var locale = 'city: oakland';
-    fetchArtistsByLocation(locale);
+    //EchoNestAPI.fetchArtistsByLocation (locale);
     
-    
+    EchoNestAPI.fetchArtistsByAppearanceDate('2015-06-11', '2015-06-19');
+                                             
     
     
 });
