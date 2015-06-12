7cebaf47cd9a242cae31e507b643936764c77e4f
guidedog51 <nordl2@pacbell.net>
2015-06-12 11:06:50 -0700

    refactor, add date functions to event lookup

--- public/scripts/echonestAPI.js
+++ public/scripts/echonestAPI.js
@@ -1,7 +1,30 @@
 var EchoNestAPI = (function () {
     jQuery.ajaxSettings.traditional = true;
        
+     //utilities
+    var _getSoundKickDateRange = function() {
+
+        var dates = [];
+        var sep = '-';
+        var today = getDayWhen(0);
+        var future = getDayWhen(7);          
+
+        dates.push(today.getFullYear() + sep + padDateItem(today.getMonth() + 1) + sep + padDateItem(today.getDate()));
+        dates.push(future.getFullYear() + sep + padDateItem(future.getMonth() + 1) + sep + padDateItem(future.getDate()));
+
+        return dates;
+
+        function getDayWhen(days) {
+            var dt = new Date();
+            dt.setDate(dt.getDate() + days);
+            return dt;
+        }
         
+        function padDateItem(item) {
+            return ('0' + item).slice(-2);
+        }
+    };
+       
      //private functions   
      var _fetchArtistsByLocation =  function (locale) {
             var url = ECHO_ENDPOINT + 'artist/search';
@@ -44,99 +67,47 @@ var EchoNestAPI = (function () {
         };
         
         var _fetchArtistsByAppearanceDate = function (start, end) {
-            var url = SOUNDKICK_ENDPOINT;
             //$("#playBar").empty();
-            
-            var reqData =                 { 
-                    'apikey' : SOUNDKICK_API_KEY,
-                    'min_date': start,  
-                    'max_date': end
-                };
 
-//            $.ajax({
-//                
-//                dataType: 'jsonp',
-//                jsonp: false,
-//                jsonpCallback: 'jsonCallback',
-//                cache: 'true',
-//                url: url,
-//                data: reqData,
-//                success: function(data) {
-//                    
-//                },
-//                error: function(xHR) {
-//                    
-//                }
-//                
-//                
-//                
-//            })
-//            
             var _renderList = function(data) {
-                //alert(data.resultsPage.status);
-                     if (data.resultsPage.status == 'ok') {
-                        var events = data.resultsPage.results.event;
-                        if (events.length > 0) {
-                            for (i = 0; i < events.length; i++) {
-                                var event = events[i].displayName;
-                                var li = $("<li>");
-                                li.text(event);
-                                $("#playBar").append(li);
-                            }
-                            
-                        } else {
-                            $('#playBar').text("No events");     
-                      }
+                 if (data.resultsPage.status == 'ok') {
+                    var events = data.resultsPage.results.event;
+                    if (events.length > 0) {
+                        for (i = 0; i < events.length; i++) {
+                            var event = events[i].displayName;
+                            var li = $("<li>");
+                            li.text(event);
+                            $("#playBar").append(li);
+                        }
+
                     } else {
-                        alert("Trouble getting artists: " + data.response.status.message);
-                    }
+                        $('#playBar').text("No events");     
+                  }
+                } else {
+                    alert("Trouble getting artists: " + data.response.status.message);
+                }
            };
             
-          //var url = "https://api.songkick.com/api/3.0/artists/253846/calendar.json?apikey=HlgKnFaq9qYO1h9T&jsoncallback=?";
-          var url = "http://api.songkick.com/api/3.0/metro_areas/26330-us-sf-bay-area/calendar.json?apikey=xdy5yMc0BaLlDZ0V&min_date=2015-06-11&max_date=2015-06-22&jsoncallback=?";
+            var url = SOUNDKICK_ENDPOINT + '&min_date=' + _getSoundKickDateRange()[0] + '&max_date=' + _getSoundKickDateRange()[1] + '&jsoncallback=?';
             $.getJSON(url, function(data){
             // Do what you want to do with the return data within this callback
                 console.log(data);
                 _renderList(data);
             });
-            
-            
-//            $.getJSON(url, 
-//                { 
-//                    'apikey' : SOUNDKICK_API_KEY,
-//                    'min_date': start,  
-//                    'max_date': end//,
-//                    //jsoncallback': 'jsonCallback'
-//                },
-//                function(data) {
-//                    if (data.resultsPage.status == 'ok') {
-//                        var events = data.resultsPage.results.event;
-//                        if (events.length > 0) {
-//                            for (i = 0; i < events.length; i++) {
-//                                var event = events[i].displayName;
-//                                var li = $("<li>");
-//                                li.text(event);
-//                                $("#playBar").append(li);
-//                            }
-//                            
-//                        } else {
-//                            $('#playBar').text("No events");     
-//                      }
-//                    } else {
-//                        alert("Trouble getting artists: " + data.response.status.message);
-//                    }
-//                })
-//                .error( 
-//                    function(data, err, response) {
-//                        eval(data.responseText);
-//                    }
-//                );
-
+//            .error( 
+//                function(data, err, response) {
+//                    eval(data.responseText);
+//                }
+//            );
         }
         
+        
+        
+        //expose public methods
         return {
             fetchArtistsByLocation: _fetchArtistsByLocation,
             fetchArtistsByAppearanceDate: _fetchArtistsByAppearanceDate
         }
-     
+        
+        
 })();
