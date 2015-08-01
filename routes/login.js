0d841f6b7c80cb09348ae5829aa74dfc2888557c
guidedog51 <nordl2@pacbell.net>
2015-08-01 11:54:45 -0700

    debug auth call

--- routes/login.js
+++ routes/login.js
@@ -8,23 +8,19 @@ router.post('/', function(req, res) {
     mongoClient.connect(config.dbUrl, function(err, mdb) {
         var col = mdb.collection('users');
         col.find({'username': req.body.username, 'password': req.body.password}).toArray(function (err, result) {
-            //console.log(col.find());
-            col.find().toArray(function (err, result) {
-                if (err) return next(err);
-                console.log(result)
-                if (result.length > 0) {
-                    req.session.auth = true;
-                    res.send(JSON.stringify({"auth": true}));
-                }
-                else {
-                    req.session.auth = false;
-                    res.send(JSON.stringify({"auth": false}));
-                }
-                mdb.close();
-            });
+            if (err) return next(err);
+            console.log(result)
+            if (result.length > 0) {
+                req.session.auth = true;
+                res.send(JSON.stringify({"auth": true}));
+            }
+            else {
+                req.session.auth = false;
+                res.send(JSON.stringify({"auth": false}));
+            }
+            mdb.close();
         });
     });
-
 });
 
 router.get('/auth', function(req, res){
