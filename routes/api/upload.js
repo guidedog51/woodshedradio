d27c7e8b92fc05e415d85944610b07f83cd2516c
guidedog51 <nordl2@pacbell.net>
2015-08-07 14:27:08 -0700

    implement azure blob storage container write

--- routes/api/upload.js
+++ routes/api/upload.js
@@ -2,52 +2,10 @@ var express = require('express');
 var upload = express.Router();
 var uploadcomplete = false;
 var multer = require('multer');
-//var app = express();
+var storage = require('azure-storage');
+var blobService = storage.createBlobService();
+var containerName = 'mpc-test-container';
 
-//app.use(multer({ dest: './uploads/',
-//    rename: function (fieldname, filename) {
-//        return filename+Date.now();
-//    },
-//    onFileUploadStart: function (file) {
-//        console.log(file.originalname + ' is starting ...')
-//    },
-//    onFileUploadComplete: function (file) {
-//        console.log(file.fieldname + ' uploaded to  ' + file.path)
-//        uploadcomplete=true;
-//    }
-//}));
-
-
-//app.use(function(req, res, next){
-//    multer({ dest: './uploads/',
-//        rename: function (fieldname, filename) {
-//            return filename+Date.now();
-//        },
-//        onFileUploadStart: function (file) {
-//            console.log(file.originalname + ' is starting ...')
-//        },
-//        onFileUploadComplete: function (file) {
-//            console.log(file.fieldname + ' uploaded to  ' + file.path)
-//            uploadcomplete=true;
-//        }
-//    })
-//})
-
-//upload.use(function(req, res, next){
-//    multer({ dest: './uploads/',
-//        rename: function (fieldname, filename) {
-//            return filename+Date.now();
-//        },
-//        onFileUploadStart: function (file) {
-//            console.log(file.originalname + ' is starting ...')
-//        },
-//        onFileUploadComplete: function (file) {
-//            console.log(file.fieldname + ' uploaded to  ' + file.path);
-//            uploadcomplete=true;
-//        }
-//    });
-//    next();
-//});
 
 /*POST new track */
 upload.post('/', [multer({ dest: './uploads/',
@@ -72,12 +30,29 @@ upload.post('/', [multer({ dest: './uploads/',
     console.log('posting file')
     if(uploadcomplete==true){
         console.log(req.files);
+        writeToBlobStorage();
         res.end("File uploaded.");
     }
 
 
 }]);
-           
+
+function writeToBlobStorage(){
+    blobService.createContainerIfNotExists(containerName, function(err, result, response) {
+        if (err) {
+            console.log("Couldn't create container %s", containerName);
+            console.error(err);
+        } else {
+            if (result) {
+                console.log('Container %s created', containerName);
+            } else {
+                console.log('Container %s already exists', containerName);
+            }
+
+            // Your code goes here
+        }
+    });
+}
 
 module.exports = upload;
 
