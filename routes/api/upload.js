1a950233c6c30044b713620602316675dd3d6efc
guidedog51 <nordl2@pacbell.net>
2015-08-07 16:43:48 -0700

    transfer uploaded file to blob storage

--- routes/api/upload.js
+++ routes/api/upload.js
@@ -5,7 +5,8 @@ var multer = require('multer');
 var storage = require('azure-storage');
 var blobService = storage.createBlobService();
 var containerName = 'mpc-test-container';
-
+var fs = require('fs');
+var uploadFilePath, uploadFileName;
 
 /*POST new track */
 upload.post('/', [multer({ dest: './uploads/',
@@ -17,6 +18,8 @@ upload.post('/', [multer({ dest: './uploads/',
         console.log(file.originalname + ' is starting ...')
     },
     onFileUploadComplete: function (file) {
+        uploadFilePath = file.path;
+        uploadFileName = file.name;
         console.log(file.fieldname + ' uploaded to  ' + file.path);
         uploadcomplete=true;
     }}),
@@ -48,8 +51,21 @@ function writeToBlobStorage(){
             } else {
                 console.log('Container %s already exists', containerName);
             }
-
-            // Your code goes here
+            //write to blob storage
+            var fileName = './' + uploadFilePath;   //uploads/favicon (2)1438969432084.ico';
+            blobService.createBlockBlobFromLocalFile(
+                containerName,
+                uploadFileName, //'mpc-test-blob.ico',
+                fileName,
+                function(error, result, response){
+                    if(error){
+                        console.log("Couldn't upload file %s", fileName);
+                        console.error(error);
+                    } else {
+                        console.log('File %s uploaded successfully', fileName);
+                    }
+                }
+            );
         }
     });
 }
