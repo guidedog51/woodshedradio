680b0efe746724a8613ee0b6bf69c3acd1ef7600
guidedog51 <nordl2@pacbell.net>
2015-10-07 14:52:34 -0700

    bind bootstrap button to fb ui method

--- public/scripts/radioplayer.js
+++ public/scripts/radioplayer.js
@@ -46,7 +46,7 @@ $(document).ready(function(){
     $('#button-fb').on('click', function(){
         FB.ui({
             method: 'share',
-            href: 'http://woodshedradio.org/radioplayer',
+            href: 'http://woodshed.cloudapp.net/radioplayer',
         }, function(response){
             //I guess we could tell 'em it didn't work....
         });
